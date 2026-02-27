127b812cd012dc76db06171e5cdd257d7887d479
Taichi <siwatpasit18@gmail.com>
2026-02-27 08:37:39 +0700

    Add cholesterol and lipid profile properties to HealthEntry

--- HealthEntry.swift
+++ HealthEntry.swift
@@ -3,9 +3,15 @@ import Foundation
 struct HealthEntry: Identifiable, Codable {
     var id = UUID()
     var date: Date
+    
     var bloodSugar: String?
     var bloodPressure: String?
-    var bodyFat: String?
+    
+    var cholesterol: String?
+    var hdl: String?
+    var ldl: String?
+    var triglyceride: String?
+    
     var bmi: Double
 }
 
