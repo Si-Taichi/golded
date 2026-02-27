64aaff306900e6632bda64d429e407e085fd4ba5
Taichi <siwatpasit18@gmail.com>
2026-02-27 15:25:45 +0700

    Add GenerationType enum for reminder categorization

--- reminder.swift
+++ reminder.swift
@@ -1,4 +1,24 @@
 import Foundation
+import SwiftUI
+
+enum GenerationType: String, CaseIterable, Identifiable, Codable {
+    case kids = "Kids"
+    case you = "You"
+    case elders = "Elders"
+    
+    var id: String { rawValue }
+    
+    var color: Color {
+        switch self {
+        case .elders:
+            return Color.yellow.opacity(0.7)
+        case .you:
+            return Color.green.opacity(0.7)
+        case .kids:
+            return Color.yellow.opacity(0.7)
+        }
+    }
+}
 
 class ReminderManager {
     
@@ -17,5 +37,17 @@ class ReminderManager {
         else { return [] }
         
         return decoded
+    }
+    
+    func load(for generation: GenerationType) -> [ReminderItem] {
+        let all = load()
+        return all.filter { $0.generation == generation }
+    }
+
+    func save(_ reminders: [ReminderItem], for generation: GenerationType) {
+        var all = load()
+        all.removeAll { $0.generation == generation }
+        all.append(contentsOf: reminders)
+        save(all)
     }
 }
